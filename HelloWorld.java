54586bb90fc0d6a4a3e1ec52597f0849754f5466
ArseniyFokin <arseniifokin@gmail.com>
2021-09-16 09:55:56 +0300

    Resolving Conflict, add full

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
 class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, world!\n");
-        System.out.println("New Feature!\n");
-=======
         System.out.println("Hello, world!1\n");
-        System.out.println("New Feature!");
->>>>>>> new-feature
+        System.out.println("New Feature!\n");
     }
 }
