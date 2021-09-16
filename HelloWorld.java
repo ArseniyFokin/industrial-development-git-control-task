fb3b381dec0291357493479eec439ae28587aa86
ArseniyFokin <arseniifokin@gmail.com>
2021-09-16 09:37:40 +0300

    Resolving conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 class Hello {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello, world!1");
+=======
+        System.out.println("Hello, world!\n");
+>>>>>>> 0e1878b (Add print \n to HelloWorld)
         System.out.println("New Feature!");
     }
 }
